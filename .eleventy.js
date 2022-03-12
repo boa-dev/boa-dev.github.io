4d630d2999f4b79968b0e39306cd6186680758f8
jasonwilliams <jase.williams@gmail.com>
2022-03-12 23:30:29 +0000

    - move to posts - add mermaid - use permalinks to keep same links - add npm ci step

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,10 @@
 const xmlFiltersPlugin = require("eleventy-xml-plugin");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const pluginMermaid = require("@kevingimbel/eleventy-plugin-mermaid");
 module.exports = function (eleventyConfig) {
+  // Mermaid
+  eleventyConfig.addPlugin(pluginMermaid);
   eleventyConfig.addPlugin(xmlFiltersPlugin);
   // RSS
   eleventyConfig.addPlugin(pluginRss);
