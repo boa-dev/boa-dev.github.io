ed234663c0e80d9b7918a350340e8f40902b8b29
Zack Mitkin <zack.mitkin@gmail.com>
2023-03-25 05:43:24 -0400

    add docusaurus and playground page

---
+++ src/pages/Playground.tsx
@@ -0,0 +1,57 @@
+import Editor from "@monaco-editor/react";
+import Layout from "@theme/Layout";
+import React from "react";
+
+const initialCode = `function greet(targetName) {
+  return 'Hello, ' + targetName + '!';
+}
+
+greet('World')
+`;
+
+export default function BoaPlayground() {
+  const [input, setInput] = React.useState(initialCode);
+  const [evaluate, setEvaluate] = React.useState(null);
+
+  console.log(evaluate, input);
+  React.useEffect(() => {
+    import("@ZackMitkin/boa").then(({ evaluate }) =>
+      setEvaluate(() => evaluate)
+    );
+  }, []);
+
+  const output = React.useMemo(() => {
+    if (evaluate !== null) {
+      try {
+        return evaluate(input);
+      } catch (e) {
+        return e;
+      }
+    }
+  }, [input, evaluate]);
+
+  return (
+    <Layout title="Playground" description="Boa Playground">
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          flexDirection: "row",
+        }}
+      >
+        <Editor
+          height="78vh"
+          theme="vs-dark"
+          language="javascript"
+          options={{ minimap: { enabled: false } }}
+          value={input}
+          onChange={setInput}
+        />
+        <div style={{ width: "30%", minWidth: "200px", paddingInline: "24px" }}>
+          <h1>Output</h1>
+          {`> ${output ?? ""}`}
+        </div>
+      </div>
+    </Layout>
+  );
+}
