340ea281a578b18fa0ac68098b0def69e8141cba
jasonwilliams <jase.williams@gmail.com>
2024-02-24 15:33:04 +0000

    style changes

--- src/components/latestPosts/latestPosts.tsx
+++ src/components/latestPosts/latestPosts.tsx
@@ -3,34 +3,41 @@ import React from "react";
 import styles from "./styles.module.css";
 
 export default function LatestPosts({ recentPosts }) {
+  console.log(recentPosts);
   return (
     <div className={clsx("container", styles.wrap)}>
-      <h2>Latest posts and feature explainers</h2>
-      <ul>
+      <h2>Latest posts</h2>
+      <section className={clsx(styles.latestPostsWrap)}>
         {recentPosts.map(({ content: BlogPostContent }) => (
           // key={BlogPostContent.metadata.permalink}
           // frontMatter={BlogPostContent.frontMatter}
           // assets={BlogPostContent.assets}
           // metadata={BlogPostContent.metadata}
           // truncated={BlogPostContent.metadata.truncated}
-          <li>
-            <a href={BlogPostContent.metadata.permalink}>
+          <a
+            className={clsx("card", styles.card)}
+            key={BlogPostContent.metadata.permalink}
+            href={BlogPostContent.metadata.permalink}
+          >
+            <div className="card__header">
               <h3>{BlogPostContent.metadata.title}</h3>
-            </a>
-            <p>
-              {new Date(BlogPostContent.metadata.date).toLocaleDateString(
-                "en-US",
-                {
-                  year: "numeric",
-                  month: "long",
-                  day: "numeric",
-                }
-              )}
-            </p>
-          </li>
+            </div>
+            <div className="card__body">
+              <p>
+                {new Date(BlogPostContent.metadata.date).toLocaleDateString(
+                  "en-US",
+                  {
+                    year: "numeric",
+                    month: "long",
+                    day: "numeric",
+                  }
+                )}
+              </p>
+            </div>
+          </a>
           //   <BlogPostContent />
         ))}
-      </ul>
+      </section>
     </div>
   );
 }
